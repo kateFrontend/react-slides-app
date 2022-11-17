3c3962d872445dc7050aa29b13e3426d29298df2
kateFrontend <ecdanilova@gmail.com>
2022-11-17 16:41:45 -0500

    button functions

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ function App() {
   const [person, setPerson] = useState(0); // display just 1st person
   const { id, name, description, age, image } = data[person];
 
+  const previousPerson = () => {
+    setPerson((person => {
+      person --;
+      if (person < 0) {
+        return data.length - 1;
+      }
+      return person;
+    }))
+  }
+
+  const nextPerson = () => {
+    setPerson((person => {
+      person ++;
+      if(person > data.length - 1) {
+        person = 0;
+      }
+      return person;
+    }))
+  }
+
   return (
     <div>
       <div className="container">
-        <img src={image} width="400px" alt="person" />
+        <img src={image} width="300px" alt="person" />
       </div>
 
       <div className="container">
@@ -23,8 +43,14 @@ function App() {
       </div>
 
       <div className="container">
-        <h2>{age}</h2>
+        <h2>{age} years old</h2>
       </div>
+
+    <div className="btn container">
+      <button onClick={previousPerson}>Previous</button>
+      <button onClick={nextPerson}>Next</button>
+    </div>
+
     </div>
   );
 }
